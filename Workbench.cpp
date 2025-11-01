fc1cae1e71e14224a9c8e20a4f92e538278bc636
Bobby Hendrich <RHendrich@gmail.com>
2025-11-01 10:32:08 -0700

    Adding call to Parsley and using the functions.

--- Workbench.cpp
+++ Workbench.cpp
@@ -1,5 +1,6 @@
 #include "Workbench.h"
 #include "Listerine.h"
+#include "Parsley.h"
 
 using namespace std;
 
@@ -51,6 +52,14 @@ int main()
 	cout << "Sol test" << endl;
 	ListNode* sol = leet.addTwoNumbers(&m3, &n3);
 	leet.showList(sol);
+
+	cout << "Parsley test" << endl;
+	Parsley shrub; 
+	stringstream sequence("3,5,9,2,7");
+	vector<string> parsed_vector = shrub.parseStringVector(sequence);
+	shrub.showStringVector(parsed_vector);
+
+
 	cout << "Did you see that?" << endl;
 }
 
