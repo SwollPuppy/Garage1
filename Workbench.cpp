1b6e143a8b26129316b01ce6c424862385282155
Bobby Hendrich <RHendrich@gmail.com>
2025-11-02 14:19:17 -0800

    Closes #16: Adding new framework for enums. This might take some tweaking in the future, but it does the work for now.

--- Workbench.cpp
+++ Workbench.cpp
@@ -4,10 +4,15 @@
 
 using namespace std;
 
+// Which part of the workbench do we want to enable?
+//bool LEGACY    = true;
+bool LINKIT    = true;
+bool PARSETEST = true;
+
 //This is where it all starts. Using header files too.
 int main()
 {
-    //The start of this thing.
+	//The start of this thing.
 	cout << "Hello World!\n";
 
 	//Function that just announces itself
@@ -30,44 +35,49 @@ int main()
 
 	showIntVector(nums);
 
-	//This is the setup for linked lists
-	//Creating first list
-	ListNode m1 = ListNode(3);
-	ListNode m2 = ListNode(4, &m1);
-	ListNode m3 = ListNode(2, &m2);
+	if (LINKIT) {
+		cout << "*** Linked List Test ***" << endl;
 
-	//Creating second list
-	ListNode n1 = ListNode(4);
-	ListNode n2 = ListNode(6, &n1);
-	ListNode n3 = ListNode(5, &n2);
+		//This is the setup for linked lists
+		//Creating first list
+		ListNode m1 = ListNode(3);
+		ListNode m2 = ListNode(4, &m1);
+		ListNode m3 = ListNode(2, &m2);
 
-	//Creating Link object to work with
-	Link leet;
-	leet.showList(&m3); //Testing the show list feature
-	leet.showList(&n3);
-	leet.showList(&m2);
-	cout << leet.lists2int(&m3) << endl;
-	cout << leet.lists2int(&n3) << endl;
+		//Creating second list
+		ListNode n1 = ListNode(4);
+		ListNode n2 = ListNode(6, &n1);
+		ListNode n3 = ListNode(5, &n2);
 
-	cout << "Sol test" << endl;
-	ListNode* sol = leet.addTwoNumbers(&m3, &n3);
-	leet.showList(sol);
+		//Creating Link object to work with
+		Link leet;
+		leet.showList(&m3); //Testing the show list feature
+		leet.showList(&n3);
+		leet.showList(&m2);
+		cout << leet.lists2int(&m3) << endl;
+		cout << leet.lists2int(&n3) << endl;
 
-	cout << "Parsley test" << endl;
-	string user_input;
-	Parsley shrub; 
-	Parsley userbush;
-	stringstream sequence("3,5,9,2,7");
-	shrub.parseStringVector(sequence);
-	shrub.showStringVector();
-	cout << "Max value is: " << shrub.maxVal() << endl;
-	cout << "Pass me a string of comma, separated integers: ";
-	cin >> user_input;
-	stringstream user_sequence(user_input);
-	userbush.parseStringVector(user_sequence);
-	userbush.showStringVector();
-	cout << "Max value is: " << userbush.maxVal() << endl;
+		cout << "Sol test" << endl;
+		ListNode* sol = leet.addTwoNumbers(&m3, &n3);
+		leet.showList(sol);
+	}
 
+	if (PARSETEST) {
+		cout << "*** Parsley Test ***" << endl;
+		string user_input;
+		Parsley shrub;
+		Parsley userbush;
+		stringstream sequence("3,5,9,2,7");
+		shrub.parseStringVector(sequence);
+		shrub.showStringVector();
+		cout << "Max value is: " << shrub.maxVal() << endl;
+		cout << "Pass me a string of comma, separated integers: ";
+		cin >> user_input;
+		stringstream user_sequence(user_input);
+		userbush.parseStringVector(user_sequence);
+		userbush.showStringVector();
+		cout << "Max value is: " << userbush.maxVal() << endl;
+	}
 
 	cout << "Did you see that?" << endl;
 }
